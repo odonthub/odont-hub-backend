75fd39168862c5c6844e8dc1cad13c8c44644ded
odonthub <odont.hub@gmail.com>
2026-05-11 21:56:50 -0300

    fix cors

--- src/plugins/cors.js
+++ src/plugins/cors.js
@@ -1,13 +1,9 @@
-// src/plugins/cors.js
 import cors from '@fastify/cors'
 import fp from 'fastify-plugin'
 
 export default fp(async (app) => {
   await app.register(cors, {
-    origin: [
-      process.env.FRONTEND_URL || 'http://localhost:5173',
-      /\.odont-hub\.com\.br$/,
-    ],
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
